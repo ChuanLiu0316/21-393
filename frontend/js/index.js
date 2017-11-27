f2d57fb19a955f74d41107121355589574b13991
Yangjun Sheng <yangjuns@andrew.cmu.edu>
2017-11-27 08:37:46 -0500

    Post data

---
+++ frontend/js/index.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $("p").click(function(){
+        $(this).hide();
+        console.log("I hide it");
+    });
+
+    $("form").submit(function (e) {
+        console.log("on submit");
+        e.preventDefault();
+        sendUserData();
+    });
+});
+
+function sendUserData(){
+    console.log("in send user data");
+    // get height
+    var height = Number($("#height").val());
+    var weight = Number($("#weight").val());
+    var age = Number($("#age").val());
+    var gender = $("#gender").val();
+    var data= new Object();
+    data.height = height;
+    data.weight = weight;
+    data.age = age;
+    data.gender = gender;
+    console.log(data);
+    var str = JSON.stringify(data);
+    console.log(str);
+    // post json to server
+    $.ajax({
+        url: 'yangjuns.info',
+        type: 'POST',
+        contentType: 'application/json',
+        data: str,
+        dataType: 'json',
+        success: function(data){
+            console.log(data);
+        }
+    });
+    alert("json posted!");
+};
