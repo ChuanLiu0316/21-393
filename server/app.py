c5d930a3fe6c494100f7853ca61676ae2a26d4b0
chuan <liuchuan95@gmail.com>
2017-11-27 13:28:09 -0500

    add placeholder for tsting frontend

--- server/app.py
+++ server/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+place_holder_response = {
+    'Panini': 2,
+    'Apple': 1,
+    'Fish': 2,
+    'Cheese Burger': 3,
+}
+
 @app.route('/')
-def hello_world():
-	return 'Hello, World'
+def index():
+	return place_holder_response
