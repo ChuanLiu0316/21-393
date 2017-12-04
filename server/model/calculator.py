d474018fa3ce62f9a36ac899976fb4bd66a2d331
Yangjun Sheng <yangjuns@andrew.cmu.edu>
2017-12-04 18:48:29 -0500

    Add calorie calculator based on activity level and Harrris-Benedict BMR formula

--- server/model/calculator.py
+++ server/model/calculator.py
@@ -19,7 +19,18 @@ def calculate(weight, height, gender, age):
     # fat
     fat = 0.25 * weight * 2.2
     # carbs
-    carb = 0.5 * calorie / 4 
+    carb = 0.5 * calorie / 4
     print "ok"
     print (5.0*calorie, 5.0*protein, 5.0*fat, 5.0*carb)
     return (4.0*calorie, 4.0*protein, 4.0*fat, 4.0*carb)
+
+def calculateCalorie(weight, height, gender, age, activity):
+    '''
+    This algorithm uses Harris–Benedict BMR with activity level factor
+    to compute calorie needed per day.
+    '''
+    if gender=="male":
+        BMR = 66.5+(13.75*weight)+(5.003*height)-(6.755*age)
+    else:
+        BMR = 655.1+(9.563*weight)+(1.850*height)-(4.676*age)
+    return activity*BMR
