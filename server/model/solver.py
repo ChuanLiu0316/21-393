5857f805853744ff08f58aa49b25355c05370879
chuan <liuchuan95@gmail.com>
2017-12-04 21:12:19 -0500

    veg

--- server/model/solver.py
+++ server/model/solver.py
@@ -17,11 +17,21 @@ nut_lower_percentage = {'Carbohydrates': 0.30, 'Protein': 0.1, 'Fat': 0.2}
 nut_higher_percentage = {'Carbohydrates': 0.6, 'Protein': 0.35, 'Fat': 0.35}
 
 def filter_al(foods, allergies):
+    #print "filter "
     def ok(food):
         if not food['Price']:
             return False
-        return True
 
+        for allergy in allergies:
+            #print allergy
+            if allergy == 'Vegetarian':
+                print food[allergy]
+                if not food[allergy] or int(food[allergy]) == 0:
+                    return False
+            else:
+                if food[allergy] and int(food[allergy]) == 1:
+                    return False
+        return True
     return [food for food in foods if ok(food)]
 
 class Solver(object):
@@ -35,12 +45,13 @@ class Solver(object):
             'Calories':float(calorie),
         }
         self.allergies = allergy
-
+        print allergy
     def run_2(self):
         # GET foods first
         try:
             foods = Food.batch_create_by_csv('data.csv')
-            foods = filter_al(foods, [])
+            print "after reading from csv"
+            foods = filter_al(foods, self.allergies)
         except Exception as e:
             print e
 
